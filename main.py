ef788e4610d3c73517784f958ff457d8f91458a9
Esoteric Enderman <90862990+EsotericEnderman@users.noreply.github.com>
2024-08-06 21:26:54 +0100

    Delete current project files

--- main.py
+++
@@ -1,6 +0,0 @@
-def on_forever():
-    basic.show_icon(IconNames.HEART, 250)
-    basic.clear_screen()
-    basic.pause(2000)
-
-basic.forever(on_forever)
--- main.ts
+++
@@ -1,5 +0,0 @@
-basic.forever(function on_forever() {
-    basic.showIcon(IconNames.Heart, 250)
-    basic.clearScreen()
-    basic.pause(2000)
-})
